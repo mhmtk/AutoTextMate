0e6a72426978d98f4f5a0dbde5199f955cffc89a
Mehmet Kologlu <kologlu.mehmet@gmail.com>
2015-03-20 17:04:53 +0200

    created add activity, made it's intent

---
+++ src/com/mhmt/autotextmate/AddRule.java
@@ -0,0 +1,15 @@
+package com.mhmt.autotextmate;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class AddRule extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_add_rule);
+	}
+}
