a1d8755fb81a71b5f6f3844971abb26fe600a0b9
Mehmet Kologlu <kologlu.mehmet@gmail.com>
2015-03-20 16:32:04 +0200

    removed hello world, added new to action bar

--- gen/com/mhmt/autotextmate/R.java
+++ gen/com/mhmt/autotextmate/R.java
@@ -1664,7 +1664,8 @@ containing a value of this type.
         public static final int action_mode_bar=0x7f09002e;
         public static final int action_mode_bar_stub=0x7f09002d;
         public static final int action_mode_close_button=0x7f090020;
-        public static final int action_settings=0x7f09003f;
+        public static final int action_new=0x7f09003f;
+        public static final int action_settings=0x7f090040;
         public static final int activity_chooser_view_content=0x7f090021;
         public static final int always=0x7f090014;
         public static final int beginning=0x7f09001b;
