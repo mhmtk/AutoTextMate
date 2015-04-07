59832a3fadbe97ffb66b6d5a50059de6c7e7c30a
Mehmet Kologlu <kologlu.mehmet@gmail.com>
2015-04-07 17:06:21 +0300

    added constructer to Rule

--- src/com/mhmt/autotextmate/dataclasses/Rule.java
+++ src/com/mhmt/autotextmate/dataclasses/Rule.java
@@ -2,4 +2,15 @@ package com.mhmt.autotextmate.dataclasses;
 
 public class Rule {
 
+	String name;
+	String description;
+	String text;
+	Boolean onlyContacts;
+	
+	public Rule(String name, String description, String text, Boolean onlyContacts) {
+		this.name = name;
+		this.description = description;
+		this.text = text;
+		this.onlyContacts = onlyContacts;
+	}
 }
