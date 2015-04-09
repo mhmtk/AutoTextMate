9fb3c4cd4b334a4e74bedbbe7c25e17c632dce3c
Mehmet Kologlu <kologlu.mehmet@gmail.com>
2015-04-09 15:34:11 +0300

    created database manager class, added it's constructor

---
+++ src/com/mhmt/autotextmate/database/DatabaseManager.java
@@ -0,0 +1,22 @@
+package com.mhmt.autotextmate.database;
+
+import java.util.ArrayList;
+
+import com.mhmt.autotextmate.dataobjects.Rule;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+
+public class DatabaseManager {
+
+	private RuleDatabaseSQLHelper dbHelper;
+	private SQLiteDatabase db;
+	private ArrayList<Rule> ruleArray;
+
+	public DatabaseManager(Context context){
+		this.dbHelper = new RuleDatabaseSQLHelper(context, "", null, 0);
+		this.db = dbHelper.getReadableDatabase();
+		this.ruleArray = new ArrayList<Rule>();
+	}
+}
