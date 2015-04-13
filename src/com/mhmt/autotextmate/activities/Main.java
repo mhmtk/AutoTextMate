ffeb3d7b1cdc546192e0c449703bd1de7a60aefb
Mehmet Kologlu <kologlu.mehmet@gmail.com>
2015-04-13 21:57:20 +0300

    added and implemented populateDbView()

--- src/com/mhmt/autotextmate/activities/Main.java
+++ src/com/mhmt/autotextmate/activities/Main.java
@@ -1,12 +1,17 @@
 package com.mhmt.autotextmate.activities;
 
 import com.mhmt.autotextmate.R;
+import com.mhmt.autotextmate.database.DatabaseManager;
+import com.mhmt.autotextmate.dataobjects.Rule;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 /**
  * 
@@ -16,6 +21,8 @@ import android.view.MenuItem;
  */
 public class Main extends ActionBarActivity {
 
+	private ListView ruleListView;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -46,6 +53,30 @@ public class Main extends ActionBarActivity {
 		}
 	}
 
+	
+	/**
+	 * populates the List View with the database data
+	 */
+	public void populateDbView(){
+		
+		//get the list view layout element
+		ruleListView = (ListView) findViewById(R.id.listview_rulelist);
+		
+		//get a database manager
+		DatabaseManager dbManager = new DatabaseManager(getApplicationContext());
+		
+		if(dbManager.getRulesArray().isEmpty()) //if the loaded database is empty
+		{
+			//error toast
+			Toast.makeText(getApplicationContext(), "Database empty! Load one or enter info!", Toast.LENGTH_SHORT).show();
+		}
+		else //make adapter with the patron array from the manager
+		{
+		ArrayAdapter<Rule> arrayAdapter = new ArrayAdapter<Rule>(this, android.R.layout.simple_list_item_1, dbManager.getRulesArray());
+		//set adapter
+		ruleListView.setAdapter(arrayAdapter);
+		}
+	}
 	private void launchAddRuleActivity() {
 		startActivity(new Intent (this, AddRule.class));
 	}
