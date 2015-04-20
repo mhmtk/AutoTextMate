fa3d29eb5823deae8e1ed82e9ed700d4a00b515c
Mehmet Kologlu <kologlu.mehmet@gmail.com>
2015-04-20 20:39:51 +0300

    Applies all status=1 rules to any incoming text and replies the rule text

--- src/com/mhmt/autotextmate/broadcastreceivers/SMSReceiver.java
+++ src/com/mhmt/autotextmate/broadcastreceivers/SMSReceiver.java
@@ -1,5 +1,8 @@
 package com.mhmt.autotextmate.broadcastreceivers;
 
+import com.mhmt.autotextmate.database.DatabaseManager;
+import com.mhmt.autotextmate.dataobjects.Rule;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -20,7 +23,9 @@ public class SMSReceiver extends BroadcastReceiver{
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		String phoneNo = "";
-		String message = "";
+//		String message = "";
+		
+		DatabaseManager dbManager = new DatabaseManager(context);
 		
 		Bundle bundle = intent.getExtras();
 		SmsMessage[] msg = null;
@@ -32,19 +37,22 @@ public class SMSReceiver extends BroadcastReceiver{
 			for (int i=0; i<msg.length; i++) {
 				msg[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
 				
-				//get the phoneNo of the sender, and the text message
+				//get the phoneNo of the sender
 				phoneNo = msg[i].getOriginatingAddress();
-				message = msg[i].getMessageBody().toString();
+//				message = msg[i].getMessageBody().toString();
 				
 			}
 			
+			//Reply
 			SmsManager smsManager = SmsManager.getDefault();
+			for (Rule r : dbManager.getApplicableRules()) {
+				String replyText = r.getText();
+				smsManager.sendTextMessage(phoneNo, null, replyText, null, null);
+				//documentation & feedback
+				Toast.makeText(context, "Replied to" + phoneNo + ": " + replyText, Toast.LENGTH_SHORT).show();
+				Log.i("SMSReceiver", "Sent out an SMS");
+			}
 			
-//			smsManager.sendTextMessage(phoneNo, null, message, null, null);
-			
-			//documentation & feedback
-			Toast.makeText(context, "ATM has replied to a SMS", Toast.LENGTH_SHORT).show();
-			Log.i("SMSReceiver", "Sent out an SMS");
 		}
 	}
 }
