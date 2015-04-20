9321cf3fcc5a338fac6444144ad9f905a5669347
Mehmet Kologlu <kologlu.mehmet@gmail.com>
2015-04-20 20:20:50 +0300

    created the getApplcableRules method for the use of SMSBroadcaster

--- src/com/mhmt/autotextmate/database/DatabaseManager.java
+++ src/com/mhmt/autotextmate/database/DatabaseManager.java
@@ -112,40 +112,42 @@ public class DatabaseManager {
 	}
 
 	public ArrayList<Rule> getApplicableRules(){
+		ruleArray = new ArrayList<Rule>();
+
+		//get readable database
+		db = dbHelper.getReadableDatabase();
+		
 		//define a projection that specifies which columns from the database to use
 				String[] projection = {
-						BaseColumns._ID,
-						RuleEntry.RULE_COLUMN_NAME,
-						RuleEntry.RULE_COLUMN_DESCRIPTION,
 						RuleEntry.RULE_COLUMN_TEXT,
 						RuleEntry.RULE_COLUMN_ONLYCONTACTS,
-						RuleEntry.RULE_COLUMN_STATUS
 				};
 
 				//sort descending
 //				String sortOrder = BaseColumns._ID + " DESC";
 
-				//create cursor with the whole database
+				//create cursor with only entries with status = 1 (on) 
 				Cursor c = db.query(
-						RuleEntry.RULE_TABLE_NAME,  // The table to query
-						projection,				// The columns to return
-						null,		            // The columns for the WHERE clause
-						null,                   // The values for the WHERE clause
-						null,                   // don't group the rows
-						null,					// don't filter by row groups
-						null	            // sort
+						RuleEntry.RULE_TABLE_NAME,  		// The table to query
+						projection,							// The columns to return
+						RuleEntry.RULE_COLUMN_STATUS + "=?",// The columns for the WHERE clause
+						new String[] { "1" },				// The values for the WHERE clause
+						null,			                    // don't group the rows
+						null,								// don't filter by row groups
+						null	            				// sort
 						);
-
+				
 				//move cursor to the beginning
 				c.moveToFirst();
 
 				while(!c.isAfterLast())
 				{ //add the rules to the arraylist
-					ruleArray.add(new Rule(c.getString(c.getColumnIndexOrThrow(RuleEntry.RULE_COLUMN_NAME)),
-							c.getString(c.getColumnIndexOrThrow(RuleEntry.RULE_COLUMN_DESCRIPTION)),
+					ruleArray.add(new Rule("",	//no name
+							"",					//no description
 							c.getString(c.getColumnIndexOrThrow(RuleEntry.RULE_COLUMN_TEXT)),
 							c.getInt(c.getColumnIndexOrThrow(RuleEntry.RULE_COLUMN_ONLYCONTACTS)),
-							c.getInt(c.getColumnIndexOrThrow(RuleEntry.RULE_COLUMN_STATUS))));
+							1					//they should all be 1 (on) anyway
+					));
 					c.moveToNext();
 				}
 
