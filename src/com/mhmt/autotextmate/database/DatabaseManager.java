6915cee0c4e480c4c4ed809b636ec6203626846f
Mehmet Kologlu <kologlu.mehmet@gmail.com>
2015-04-20 19:57:34 +0300

    minor optimization

--- src/com/mhmt/autotextmate/database/DatabaseManager.java
+++ src/com/mhmt/autotextmate/database/DatabaseManager.java
@@ -30,9 +30,9 @@ public class DatabaseManager {
 	 */
 	public DatabaseManager(Context context){
 		this.dbHelper = new RuleDatabaseSQLHelper(context, "", null, 0);
-		
+
 	}
-	
+
 	/**
 	 * Adds the given rule to the database
 	 * 
@@ -43,7 +43,7 @@ public class DatabaseManager {
 
 		//get writeable database
 		db = dbHelper.getWritableDatabase();
-		
+
 		// map of values
 		ContentValues values = new ContentValues();
 		values.put(RuleEntry.RULE_COLUMN_NAME, rule.getName());
@@ -54,7 +54,7 @@ public class DatabaseManager {
 		// Insert the new row
 		db.insertOrThrow(RuleEntry.RULE_TABLE_NAME, null, values);
 		RuleDatabaseSQLHelper.INITIALIZED = true;
-		
+
 		db.close(); //close database 
 	}
 
@@ -65,7 +65,7 @@ public class DatabaseManager {
 	 */
 	public ArrayList<Rule> getRulesArray() {
 		ruleArray = new ArrayList<Rule>();
-		
+
 		//get readable database
 		db = dbHelper.getReadableDatabase();
 		//define a projection that specifies which columns from the database to use
@@ -106,8 +106,50 @@ public class DatabaseManager {
 			ruleArray.add(p);
 			c.moveToNext();
 		}
-		
+
 		db.close();
 		return ruleArray;
+	}
+
+	public ArrayList<Rule> getApplicableRules(){
+		//define a projection that specifies which columns from the database to use
+				String[] projection = {
+						BaseColumns._ID,
+						RuleEntry.RULE_COLUMN_NAME,
+						RuleEntry.RULE_COLUMN_DESCRIPTION,
+						RuleEntry.RULE_COLUMN_TEXT,
+						RuleEntry.RULE_COLUMN_ONLYCONTACTS,
+						RuleEntry.RULE_COLUMN_STATUS
+				};
+
+				//sort descending
+//				String sortOrder = BaseColumns._ID + " DESC";
+
+				//create cursor with the whole database
+				Cursor c = db.query(
+						RuleEntry.RULE_TABLE_NAME,  // The table to query
+						projection,				// The columns to return
+						null,		            // The columns for the WHERE clause
+						null,                   // The values for the WHERE clause
+						null,                   // don't group the rows
+						null,					// don't filter by row groups
+						null	            // sort
+						);
+
+				//move cursor to the beginning
+				c.moveToFirst();
+
+				while(!c.isAfterLast())
+				{ //add the rules to the arraylist
+					ruleArray.add(new Rule(c.getString(c.getColumnIndexOrThrow(RuleEntry.RULE_COLUMN_NAME)),
+							c.getString(c.getColumnIndexOrThrow(RuleEntry.RULE_COLUMN_DESCRIPTION)),
+							c.getString(c.getColumnIndexOrThrow(RuleEntry.RULE_COLUMN_TEXT)),
+							c.getInt(c.getColumnIndexOrThrow(RuleEntry.RULE_COLUMN_ONLYCONTACTS)),
+							c.getInt(c.getColumnIndexOrThrow(RuleEntry.RULE_COLUMN_STATUS)));
+					c.moveToNext();
+				}
+
+				db.close();
+				return ruleArray;
 	}
 }
