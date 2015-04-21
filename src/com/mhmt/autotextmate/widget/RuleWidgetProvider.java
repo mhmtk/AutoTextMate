3d7a7cda4e05c094d7aa92cea994b1438e8c8f06
Mehmet Kologlu <kologlu.mehmet@gmail.com>
2015-04-21 18:40:09 +0300

    added loop and test toast to the onUpdate

--- src/com/mhmt/autotextmate/widget/RuleWidgetProvider.java
+++ src/com/mhmt/autotextmate/widget/RuleWidgetProvider.java
@@ -1,12 +1,28 @@
 package com.mhmt.autotextmate.widget;
 
+import com.mhmt.autotextmate.R;
+
+import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.widget.EditText;
+import android.widget.RemoteViews;
+import android.widget.Toast;
 
 public class RuleWidgetProvider extends AppWidgetProvider {
 
-	public void onUpdate(Context context, AppWidgetProvider appWidgetManager, int[] appWidgetIds){
+	@Override
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds){
+		super.onUpdate(context, appWidgetManager, appWidgetIds);
 		
+		for (int i=0; i<appWidgetIds.length; i++) {
+			int appWidgetId = appWidgetIds[i];
+			
+			Toast.makeText(context, "updated widget " + i, Toast.LENGTH_SHORT).show();
+			
+			RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.layout_widget);
+			
+		}
 	}
 
 }
