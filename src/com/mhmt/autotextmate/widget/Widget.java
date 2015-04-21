1b0231b4046e5ad9cae647d96c98832e88717795
Mehmet Kologlu <kologlu.mehmet@gmail.com>
2015-04-21 18:02:29 +0300

    created the source java class for the widgets

---
+++ src/com/mhmt/autotextmate/widget/Widget.java
@@ -0,0 +1,12 @@
+package com.mhmt.autotextmate.widget;
+
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+public class Widget extends AppWidgetProvider {
+
+	public void onUpdate(Context context, AppWidgetProvider appWidgetManager, int[] appWidgetIds){
+		
+	}
+
+}
