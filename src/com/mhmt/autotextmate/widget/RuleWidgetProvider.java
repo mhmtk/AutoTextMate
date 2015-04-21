16c6ab1c0058e676f8676a83ef448e7691a62081
Mehmet Kologlu <kologlu.mehmet@gmail.com>
2015-04-21 18:44:08 +0300

    added o'clock pending intent so that the button launches the app

--- src/com/mhmt/autotextmate/widget/RuleWidgetProvider.java
+++ src/com/mhmt/autotextmate/widget/RuleWidgetProvider.java
@@ -1,27 +1,34 @@
 package com.mhmt.autotextmate.widget;
 
 import com.mhmt.autotextmate.R;
+import com.mhmt.autotextmate.activities.Main;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
-import android.widget.EditText;
+import android.content.Intent;
 import android.widget.RemoteViews;
-import android.widget.Toast;
 
 public class RuleWidgetProvider extends AppWidgetProvider {
 
 	@Override
 	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds){
 		super.onUpdate(context, appWidgetManager, appWidgetIds);
-		
+
 		for (int i=0; i<appWidgetIds.length; i++) {
 			int appWidgetId = appWidgetIds[i];
-			
-			Toast.makeText(context, "updated widget " + i, Toast.LENGTH_SHORT).show();
-			
+
+			// Create an Intent to launch ExampleActivity
+			Intent intent = new Intent(context, Main.class);
+			PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+			// Get the layout for the App Widget and attach an on-click listener to the button
 			RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.layout_widget);
-			
+			views.setOnClickPendingIntent(R.id.widget_button, pendingIntent);
+
+			//perform an update on teh current app widget
+			appWidgetManager.updateAppWidget(appWidgetId, views);
 		}
 	}
 
