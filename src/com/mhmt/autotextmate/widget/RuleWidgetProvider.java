1ae61f1c780137ec94c215cdebebaeeaef89f778
Mehmet Kologlu <kologlu.mehmet@gmail.com>
2015-04-21 18:03:17 +0300

    renamed to rulewidgetprovider

--- src/com/mhmt/autotextmate/widget/RuleWidgetProvider.java
+++ src/com/mhmt/autotextmate/widget/RuleWidgetProvider.java
@@ -3,7 +3,7 @@ package com.mhmt.autotextmate.widget;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 
-public class Widget extends AppWidgetProvider {
+public class RuleWidgetProvider extends AppWidgetProvider {
 
 	public void onUpdate(Context context, AppWidgetProvider appWidgetManager, int[] appWidgetIds){
 		
