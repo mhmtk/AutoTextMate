7ed3d491d4599640a2df71dae30b32be83a93b82
Mehmet Kologlu <kologlu.mehmet@gmail.com>
2015-04-22 21:01:22 +0300

    removed unnecessary imports

--- src/com/mhmt/autotextmate/widget/ConfigureWidget.java
+++ src/com/mhmt/autotextmate/widget/ConfigureWidget.java
@@ -1,12 +1,9 @@
 package com.mhmt.autotextmate.widget;
 
 import com.mhmt.autotextmate.R;
-import com.mhmt.autotextmate.R.layout;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class ConfigureWidget extends Activity {
 
