5e5e7a4700865a080953314dba5e93880104407d
Mehmet Kologlu <kologlu.mehmet@gmail.com>
2015-05-05 19:36:29 +0300

    populateListView now sets the adapter even if the data is empty

--- src/com/mhmt/autotextmate/activities/Main.java
+++ src/com/mhmt/autotextmate/activities/Main.java
@@ -31,6 +31,7 @@ public class Main extends ActionBarActivity {
 	private DatabaseManager dbManager;
 	private ArrayList<Rule> ruleArray;
 	private String logTag = "Main";
+	private RuleListViewAdapter mListAdapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -57,15 +58,14 @@ public class Main extends ActionBarActivity {
 
 		if(ruleArray.isEmpty()) //if the loaded database is empty
 		{
-			//error toast
+			//Feedback
 			Toast.makeText(getApplicationContext(), "You have no saved rules to view", Toast.LENGTH_SHORT).show();
 		}
-		else //there's stuff in the DB, go ahead and populate the view
-		{	
-			//pass the adapter with the array to the list view
-			ruleListView.setAdapter(new RuleListViewAdapter(this, ruleArray, getResources()));
-			Log.i(logTag, "adapter to rulelistview set");
-		}
+
+		//pass the adapter with the array to the list view
+		mListAdapter = new RuleListViewAdapter(this, ruleArray, getResources());
+		ruleListView.setAdapter(mListAdapter);
+		Log.i(logTag, "adapter to rulelistview set");
 	}
 
 	@Override
@@ -107,19 +107,19 @@ public class Main extends ActionBarActivity {
 		startActivity(new Intent (this, AddRule.class));
 	}
 
-//	/**
-//	 * onClick of each row of the listView, called thru the RuleListViewAdapter
-//	 * 
-//	 * @param mPosition the position of the item on the list, 0 indexed
-//	 */
-//	public void onItemClick(int mPosition)
-//	{
-//		//documentation and feedback
-//		Log.i(logTag, "Item " + mPosition + " clicked.");
-//		Toast.makeText(getApplicationContext(), "Item " + mPosition + " clicked.",Toast.LENGTH_LONG).show();
-//		// Edit window?
-//		// more info?
-//	}
+	//	/**
+	//	 * onClick of each row of the listView, called thru the RuleListViewAdapter
+	//	 * 
+	//	 * @param mPosition the position of the item on the list, 0 indexed
+	//	 */
+	//	public void onItemClick(int mPosition)
+	//	{
+	//		//documentation and feedback
+	//		Log.i(logTag, "Item " + mPosition + " clicked.");
+	//		Toast.makeText(getApplicationContext(), "Item " + mPosition + " clicked.",Toast.LENGTH_LONG).show();
+	//		// Edit window?
+	//		// more info?
+	//	}
 
 	/**
 	 * onClick-ish method for the togglebutton in each row of the listView, 
@@ -135,7 +135,7 @@ public class Main extends ActionBarActivity {
 
 		//Change the status of the rule in the database-
 		int wID = dbManager.setRuleStatus(mName, isChecked);
-		
+
 		if (wID != AppWidgetManager.INVALID_APPWIDGET_ID) {
 			//Send a broadcast for the widget to update itself
 			Intent updateWidgetIntent = new Intent();
@@ -146,7 +146,7 @@ public class Main extends ActionBarActivity {
 		}
 		else
 			Log.i(logTag, "Did not broadcast widget update b/c " + mName + " has no widget");
-		
+
 	}
 
 	/**
@@ -156,34 +156,35 @@ public class Main extends ActionBarActivity {
 	 */
 	public void onLongItemClick(final String ruleName) {
 		Log.i(logTag, "Long click detected at " + ruleName);
-		
-//		Bundle bundle = new Bundle();
-//		bundle.putString("ruleName", String.valueOf(mPosition));
-//		EditDeleteRuleDialogFragment fragment = new EditDeleteRuleDialogFragment();
-//		fragment.setArguments(bundle);
-//		fragment.show();
-		
+
+		//		Bundle bundle = new Bundle();
+		//		bundle.putString("ruleName", String.valueOf(mPosition));
+		//		EditDeleteRuleDialogFragment fragment = new EditDeleteRuleDialogFragment();
+		//		fragment.setArguments(bundle);
+		//		fragment.show();
+
 		new AlertDialog.Builder(this)
-	    .setTitle(ruleName)
-	    .setPositiveButton(R.string.dialog_edit, new DialogInterface.OnClickListener() {
-	        public void onClick(DialogInterface dialog, int which) { 
-	            // TODO open edit activity
-	        }
-	     })
-	    .setNegativeButton(R.string.dialog_delete, new DialogInterface.OnClickListener() {
-	        public void onClick(DialogInterface dialog, int which) {
-	        	deleteRule(ruleName);
-	        }
-	     })
-//	    .setIcon(android.R.drawable.ic_dialog_alert)
-	    .show();
+		.setTitle(ruleName)
+		.setPositiveButton(R.string.dialog_edit, new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int which) { 
+				// TODO open edit activity
+			}
+		})
+		.setNegativeButton(R.string.dialog_delete, new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int which) {
+				deleteRule(ruleName);
+			}
+		})
+		//	    .setIcon(android.R.drawable.ic_dialog_alert)
+		.show();
 	}
-	
+
 	public void deleteRule(String ruleName){
 		// TODO remove the rules widget
-		
-        // TODO delete from DB / reconstruct view
-    	dbManager.deleteRule(ruleName);
+
+		// TODO delete from DB / reconstruct view
+		dbManager.deleteRule(ruleName);
+		mListAdapter.notifyDataSetChanged();
 	}
 
 }
