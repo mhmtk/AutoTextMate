3d393ed671e9b0dbc17a02d275412edde493f8ed
Mehmet Kologlu <kologlu.mehmet@gmail.com>
2015-05-18 22:59:09 +0300

    Implemented getting an SMSArray and setting the adapter

--- src/com/mhmt/autotextmate/activities/Outbox.java
+++ src/com/mhmt/autotextmate/activities/Outbox.java
@@ -1,15 +1,30 @@
 package com.mhmt.autotextmate.activities;
 
+import java.util.ArrayList;
+
 import com.mhmt.autotextmate.R;
+import com.mhmt.autotextmate.database.DatabaseManager;
+import com.mhmt.autotextmate.dataobjects.SMS;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
 public class Outbox extends ListActivity {
+
+	private DatabaseManager dbManager;
+	private ArrayList<SMS> smsArray;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_outbox);
+
+		// instantiate fields
+		dbManager = new DatabaseManager(getApplicationContext());
+		smsArray = dbManager.getSMSArray();
+
+		// Set the list adapter
+		setListAdapter(new ArrayAdapter<SMS>(this, android.R.layout.simple_list_item_1, smsArray));
 	}
 }
