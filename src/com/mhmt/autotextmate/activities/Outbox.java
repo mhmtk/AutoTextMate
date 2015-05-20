4ce766c595a6377a96eeffa4875fca25f885ea38
Mehmet Kologlu <kologlu.mehmet@gmail.com>
2015-05-20 18:31:00 +0300

    Outbox now extends ActionBarActivity and uses a listFragment for the list, this enables using an appcompat theme

--- src/com/mhmt/autotextmate/activities/Outbox.java
+++ src/com/mhmt/autotextmate/activities/Outbox.java
@@ -6,10 +6,15 @@ import com.mhmt.autotextmate.R;
 import com.mhmt.autotextmate.database.DatabaseManager;
 import com.mhmt.autotextmate.dataobjects.SMS;
 
-import android.app.ListActivity;
-import android.graphics.drawable.ColorDrawable;
+import android.support.v4.app.ListFragment;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 /**
  * 
@@ -17,24 +22,41 @@ import android.widget.ArrayAdapter;
  * @version November May 20, 2015
  * 
  */
-public class Outbox extends ListActivity {
+public class Outbox extends ActionBarActivity {
 
-	private DatabaseManager dbManager;
-	private ArrayList<SMS> smsArray;
+	
+	private static Context context;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_outbox);
+		context = this;
 
-		getActionBar().setIcon(new ColorDrawable(getResources().getColor(android.R.color.transparent)));
-		getActionBar().setDisplayHomeAsUpEnabled(true);
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+		mListFragment fragment = new mListFragment();
+        getSupportFragmentManager().beginTransaction().replace(android.R.id.content, fragment).commit();
+	}
+
+	public static class mListFragment extends ListFragment {
+
+		private DatabaseManager dbManager;
+		private ArrayList<SMS> smsArray;
 		
-		// instantiate fields
-		dbManager = new DatabaseManager(getApplicationContext());
-		smsArray = dbManager.getSMSArray();
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+	        return inflater.inflate(R.layout.activity_outbox, container, false);
+	    }
+		
+		public void onActivityCreated(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+			// Set the list adapter
+			dbManager = new DatabaseManager(context);
+			smsArray = dbManager.getSMSArray();
+			setListAdapter(new ArrayAdapter<SMS>(context, android.R.layout.simple_list_item_1, smsArray));
+		}
 
-		// Set the list adapter
-		setListAdapter(new ArrayAdapter<SMS>(this, android.R.layout.simple_list_item_1, smsArray));
+		public void onListItemClick(ListView listView, View view, int position, long id) {
+		}
 	}
 }
