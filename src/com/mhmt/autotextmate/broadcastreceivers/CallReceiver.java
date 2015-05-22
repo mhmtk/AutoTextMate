ec0c94e14e2694f595802e9ac25cd4faf6348edb
Mehmet Kologlu <kologlu.mehmet@gmail.com>
2015-05-23 02:36:48 +0300

    Call receiver now uses the SPref muteDelay to mute a call after a certain time or not mute at all

--- src/com/mhmt/autotextmate/broadcastreceivers/CallReceiver.java
+++ src/com/mhmt/autotextmate/broadcastreceivers/CallReceiver.java
@@ -1,5 +1,6 @@
 package com.mhmt.autotextmate.broadcastreceivers;
 
+import com.mhmt.autotextmate.R;
 import com.mhmt.autotextmate.database.DatabaseManager;
 import com.mhmt.autotextmate.dataobjects.Rule;
 import com.mhmt.autotextmate.dataobjects.SMS;
@@ -8,9 +9,11 @@ import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.media.AudioManager;
 import android.net.Uri;
+import android.os.Handler;
 import android.provider.BaseColumns;
 import android.provider.ContactsContract;
 import android.telephony.PhoneStateListener;
@@ -32,6 +35,9 @@ public class CallReceiver extends BroadcastReceiver{
 	private SmsManager smsManager = SmsManager.getDefault();
 	private AudioManager aManager;
 
+	private SharedPreferences sharedPref;
+	private SharedPreferences.Editor sPrefEditor;
+	
 	private static MPhoneStateListener phoneListener;
 
 	@Override
@@ -45,8 +51,12 @@ public class CallReceiver extends BroadcastReceiver{
 		if (phoneListener == null) {
 			dbManager = new DatabaseManager(context);
 			phoneListener = new MPhoneStateListener(context);
+			//Get audio manager for mute option
 			aManager = (AudioManager) context
 	                .getSystemService(Context.AUDIO_SERVICE);
+			//Get shared preferences
+			sharedPref = context.getSharedPreferences(context.getString(R.string.shared_preferences_key),Context.MODE_PRIVATE);
+			sharedPref.edit();
 			telephony.listen(phoneListener,PhoneStateListener.LISTEN_CALL_STATE);
 		}
 	}
@@ -56,7 +66,7 @@ public class CallReceiver extends BroadcastReceiver{
 		// Flag used to avoid invoking rules multiple times due to receiving more than one one RINGING state change
 		private boolean handled = false;
 		private Context mContext;
-		
+		private int muteDelay;
 
 		private MPhoneStateListener(Context c) {
 			mContext = c;
@@ -80,6 +90,10 @@ public class CallReceiver extends BroadcastReceiver{
 				Log.i(logTag, "RINGING");
 				if (!handled) { // In order 
 					Log.i(logTag, "Call hasn't been handled, will invoke applicable rules");
+					
+					// Get mute time
+					muteDelay = sharedPref.getInt(mContext.getString(R.string.settings_mute_key), -1);
+					
 					for (Rule r : dbManager.getEnabledCallRules()) { //Reply for each rule
 						if (r.getOnlyContacts() == 1) { // Reply only if the sender no is in the contacts
 							if (inContacts(mContext, incomingNumber)) { // Check if the sender is in the contacts
@@ -118,8 +132,16 @@ public class CallReceiver extends BroadcastReceiver{
 			Toast.makeText(mContext, "Replied to " + phoneNo + ": " + replyText, Toast.LENGTH_SHORT).show();
 			Log.i(logTag, "Sent out an SMS to " + phoneNo);
 			
-			aManager.setStreamMute(AudioManager.STREAM_RING, true);
-			Log.i(logTag, "Muted");
+			// Runnable for delayed muting
+			if (muteDelay != -1) {
+				new Handler().postDelayed(new Runnable() {
+					public void run() {
+						aManager.setStreamMute(AudioManager.STREAM_RING, true);
+						Log.i(logTag, "Ringer muted after " + muteDelay + " seconds.");
+					} 
+				}, muteDelay );
+			}
+			
 		}
 	}
 	
