b9dfda678d65c0cd05c800f9895a2db70c238090
Mehmet Kologlu <kologlu.mehmet@gmail.com>
2015-05-25 18:40:50 +0300

    Added millisecond to second conversion, added logging

--- src/com/mhmt/autotextmate/activities/Settings.java
+++ src/com/mhmt/autotextmate/activities/Settings.java
@@ -6,6 +6,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -14,7 +15,7 @@ import android.widget.Spinner;
 /**
  * 
  * @author Mehmet Kologlu
- * @version November May 23, 2015
+ * @version November May 25, 2015
  * 
  */
 public class Settings extends Activity {
@@ -22,43 +23,48 @@ public class Settings extends Activity {
 	private String[] spinnerArray;
 	private SharedPreferences sharedPref;
 	private SharedPreferences.Editor sPrefEditor;
-	
+	private String logTag = "Settings";
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_settings);
 
-		// Instantiate fields
+		// Instantiate view fields
 		Spinner muteSpinner = (Spinner) findViewById(R.id.settings_spinner_mute);
-		
+
 		// Get shared preferences, and it's editor
 		sharedPref = getSharedPreferences(getString(R.string.shared_preferences_key),Context.MODE_PRIVATE);
-		sharedPref.edit();
-		
-		// Get spinner array from resources
+		sPrefEditor = sharedPref.edit();
+
+		// Instantiate data fields
 		spinnerArray = getResources().getStringArray(R.array.settings_mute_spinner_array);
-		
+
 		// Create an array adapter for the usage with the mute spinner
 		ArrayAdapter<CharSequence> muteAdapter = ArrayAdapter.createFromResource(this,
-		        R.array.settings_mute_spinner_array, android.R.layout.simple_spinner_item);
+				R.array.settings_mute_spinner_array, android.R.layout.simple_spinner_item);
 		// Specify the layout to use when the list of choices appears
 		muteAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		// Apply the adapter to the spinner
 		muteSpinner.setAdapter(muteAdapter);
-		
+
 		// Set onItemSelectedListener of the mute spinner
 		muteSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
 			@Override
 			public void onItemSelected(AdapterView<?> parent, View view,
 					int position, long id) {
+				Log.i(logTag, "Mute spinner - onItemSelected called with position: " + position);
 				if (position !=0) {
-				sPrefEditor.putInt(getString(R.string.settings_mute_key), Integer.valueOf(spinnerArray[position]));
-				sPrefEditor.commit();
+					sPrefEditor.putInt(getString(R.string.settings_mute_key), Integer.valueOf(spinnerArray[position]) * 1000); // convert to seconds
+					if (!sPrefEditor.commit()) {
+						Log.e(logTag, "Error while committing mute delay into shared preferences");
+					}
 				}
 			}
 
 			@Override
 			public void onNothingSelected(AdapterView<?> parent) {
+				Log.i(logTag, "Mute spinner - onNothingSelected called");
 			}
 		});
 	}
